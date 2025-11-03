0e02003df9d41139e1dc57bb921a921d8606ffa3
Kaizenae <a.saed@kaizenae.com>
2025-11-03 12:05:29 +0400

    edit the manifest on kaz_wizard_messages

--- kaz_wizard_messages/__manifest__.py
+++ kaz_wizard_messages/__manifest__.py
@@ -18,6 +18,6 @@
 	"data": [
 		'security/ir.model.access.csv',
 		'wizard/wizard_message.xml'],
-	"images": ['static/description/Banner.png'],
+	"images": ['static/description/banner.png'],
 	"pre_init_hook": "pre_init_check",
 }
