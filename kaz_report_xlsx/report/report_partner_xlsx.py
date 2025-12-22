c938e64e8be0954ba0878f1381c4455f9b03f22f
Kaizenae <a.saed@kaizenae.com>
2025-12-22 14:14:25 +0400

    add kaz_report_xlsx

---
+++ kaz_report_xlsx/report/report_partner_xlsx.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Kaizen Principles
+
+from odoo import models
+
+
+class PartnerXlsx(models.AbstractModel):
+    _name = "report.kaz_report_xlsx.partner_xlsx"
+    _inherit = "report.kaz_report_xlsx.abstract"
+    _description = "Partner XLSX Report"
+
+    def generate_xlsx_report(self, workbook, data, partners):
+        sheet = workbook.add_worksheet("Report")
+        for i, obj in enumerate(partners):
+            bold = workbook.add_format({"bold": True})
+            sheet.write(i, 0, obj.name, bold)
