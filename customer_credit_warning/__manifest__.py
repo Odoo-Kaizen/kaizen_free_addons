50a349b739bf8e6e72ce5a12c19dee3910b939c7
Kaizenae <a.saed@kaizenae.com>
2026-01-16 10:50:51 +0400

    add v

---
+++ customer_credit_warning/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Customer Credit Warning",
+
+    'summary': """
+       Customer Credit Warning, Customer Due Payments, Customer Customer Due Payments in Sale Order, Sale Order Customer Credit Warning""",
+
+    'description': """
+       Customer Credit Warning, Customer Due Payments, Customer Customer Due Payments in Sale Order, Sale Order Customer Credit Warning """,
+
+    'author': "Kaizen Principles",
+    'website': 'http://www.kaizenae.com',
+
+    'category': 'Accounting',
+    'version': '19.0.1.0.1',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account_accountant', 'sale'],
+
+    # always loaded
+    'data': [
+        'views/sale_order.xml',
+    ],
+
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+
+}
