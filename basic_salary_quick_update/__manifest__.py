48adbad6c6ef94f477e1134ac0f66392fff138cf
Kaizenae <a.saed@kaizenae.com>
2026-01-16 16:34:49 +0400

    add basic_salary_quick_update

--- basic_salary_quick_update/__manifest__.py
+++ basic_salary_quick_update/__manifest__.py
@@ -10,10 +10,10 @@
     """,
 
     'author': "Kaizen Principles",
-    'website': "https://erp-software.odoo-saudi.com/discount/",
+    'website': "https://kaizenae.com",
 
     'category': 'HR',
-    'version': '17.0.0.1',
+    'version': '17.0.1.0.1',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
