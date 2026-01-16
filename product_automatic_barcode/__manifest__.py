04aad80eaca0689cc51b8afbee30231ec9c17f65
Kaizenae <a.saed@kaizenae.com>
2026-01-16 11:14:48 +0400

    add product_automatic_barcode

--- product_automatic_barcode/__manifest__.py
+++ product_automatic_barcode/__manifest__.py
@@ -15,7 +15,7 @@
     'website': "https://erp-software.odoo-saudi.com/discount/",
 
     'category': 'Inventory',
-    'version': '17.0.0.0.1',
+    'version': '17.0.1.0.1',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
